303598791b846b4c1a43ded19009a44df738ff7d
Jason Lee <79300253+junybike@users.noreply.github.com>
2024-12-19 17:24:01 -0700

    Hovering Mushroom v2 Re-structured

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,16 @@
 #include "SFML/Graphics/Sprite.hpp"
 #include "SFML/Graphics/Texture.hpp"
 
+#include "../include/game.h"
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Mushroom");
+    Game game;
 
-    sf::Texture mushroomTexture;
-    mushroomTexture.loadFromFile("mushroom.png");
-    sf::Sprite mushroom(mushroomTexture);
-    sf::Vector2u size = mushroomTexture.getSize();
-    mushroom.setOrigin(size.x / 2, size.y / 2);
-    sf::Vector2f increment(1.0f, 1.0f);
-
-    while (window.isOpen())
+    while (!game.GetWindow()->IsDone())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed) window.close();
-        }
-        if ((mushroom.getPosition().x + (size.x / 2) > window.getSize().x && increment.x > 0) ||
-            (mushroom.getPosition().x - (size.x / 2) < 0 && increment.x < 0))
-        {
-            increment.x = -increment.x;
-        }
-        if ((mushroom.getPosition().y + (size.y / 2) > window.getSize().y && increment.y > 0) ||
-            (mushroom.getPosition().y - (size.y / 2) < 0 && increment.y < 0))
-        {
-            increment.y = -increment.y;
-        }
-        mushroom.setPosition(mushroom.getPosition() + increment);
-
-        window.clear(sf::Color(16,16,16,255));
-        window.draw(mushroom);
-        window.display();
+        game.Update();
+        game.Render();
     }
 }
+
